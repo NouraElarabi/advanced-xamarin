8f1b3ab315438ffe2fa9f9e0be7bfbdbc61bd0df
Noura Elarabi <NElarabi@integrant.com>
2019-07-25 14:43:52 +0300

    Section 5: Behaviors and Effects

---
+++ ExpensesApp/ExpensesApp/ExpensesApp.iOS/Effects/SelectedEffect.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using ExpensesApp.iOS.Effects;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName("CompanyName")] // this line is set only once per platform
+[assembly: ExportEffect(typeof(SelectedEffect), "SelectedEffect")] // the name after companyName.(..)
+namespace ExpensesApp.iOS.Effects
+{
+    public class SelectedEffect : PlatformEffect
+    {
+        UIColor selectedColor;
+
+        protected override void OnAttached()
+        {
+            selectedColor = new UIColor(176 / 255, 152 / 255, 164 / 255, 255 / 255);
+        }
+
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+
+            if (args.PropertyName == "IsFocused")
+            {
+                if (Control.BackgroundColor != selectedColor)
+                {
+                    Control.BackgroundColor = selectedColor;
+                }
+                else
+                {
+                    Control.BackgroundColor = UIColor.White;
+                }
+            }
+        }
+
+        protected override void OnDetached()
+        {
+
+        }
+    }
+}
