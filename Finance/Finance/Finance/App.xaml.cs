0f0845f874492fd53b87b7b9863677ec5cc0d3f9
Noura Elarabi <NElarabi@integrant.com>
2019-07-27 19:02:49 +0300

    crashes and analytics

--- Finance/Finance/Finance/App.xaml.cs
+++ Finance/Finance/Finance/App.xaml.cs
@@ -2,6 +2,9 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Finance.View;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Finance
@@ -18,6 +21,12 @@ namespace Finance
         protected override void OnStart()
         {
             // Handle when your app starts
+            AppCenter.Start("android=d2808417-4efe-42de-ba80-237001d364f1;" +
+                            "ios=15034a68-c0f0-49b0-b9b4-a4325f588d7c",
+                typeof(Analytics), typeof(Crashes));
+            // we can put this line between InitializeComponent and MainPage assignment if we wanted to start collecting crashes earlier
+
+            handleCrash();
         }
 
         protected override void OnSleep()
@@ -28,6 +37,15 @@ namespace Finance
         protected override void OnResume()
         {
             // Handle when your app resumes
+        }
+
+        private async void handleCrash()
+        {
+            bool didAppCrash = await Crashes.HasCrashedInLastSessionAsync();
+            if (didAppCrash)
+            {
+                var crashReport = await Crashes.GetLastSessionCrashReportAsync();// we can use the report data to give the user more info or do some action
+            }
         }
     }
 }
--- Finance/Finance/Finance/View/PostPage.xaml.cs
+++ Finance/Finance/Finance/View/PostPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using Finance.Model;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Xamarin.Forms;
 
 namespace Finance.View
@@ -17,10 +19,44 @@ namespace Finance.View
         public PostPage(Item item)
         {
             InitializeComponent();
+
             Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
             //reserves the home button area at the buttom for newer iphones without content
+            try
+            {
+                var properties = new Dictionary<string, string>()
+                {
+                    {"Blog_Post", item.Title}
+                };
+                webView.Source = item.ItemLink;
+                throw (new Exception("unable to load blog"));
+            }
+            catch (Exception ex)
+            {
+                var properties = new Dictionary<string, string>()
+                {
+                    {"Blog_Post", item.Title}
+                };
+                TrackError(ex, properties);
+                Crashes.GenerateTestCrash(); // this will actually crash the app so next time HasCrashedInLastSessionAsync would return true
+            }
+            
+        }
 
-            webView.Source = item.ItemLink;
+        private async void TrackEvent(Dictionary<string, string> properties)
+        {
+            if (await Analytics.IsEnabledAsync())
+            {
+                Analytics.TrackEvent("Blog_Post_Being_Opened", properties);
+            }
+        }
+
+        private async void TrackError(Exception ex,Dictionary<string, string> properties)
+        {
+            if (await Crashes.IsEnabledAsync())
+            {
+                Crashes.TrackError(ex, properties);
+            }
         }
     }
 }
