2932e1c93a1157b060b4fd50c81792af3923097f
Noura Elarabi <NElarabi@integrant.com>
2019-08-11 01:54:06 +0300

    Section 10: Advanced Xamarin Android

---
+++ ContactsApp/ContactsApp/ContactsApp.Android/Contact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ContactsApp.Droid;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Contact))]
+
+namespace ContactsApp.Droid
+{
+    [Android.Runtime.Preserve(AllMembers =
+        false)] // only members that are marked with Conditional = false will always be preserved
+    public class Contact : IContact
+    {
+        //[Android.Runtime.Preserve]
+        [Android.Runtime.Preserve(Conditional = false)]
+        public Contact()
+        {
+            // code that I want to preserve
+        }
+
+        public string ByeContact()
+        {
+            Contacts.Contact contact = new Contacts.Contact("Eduardo", "Rosas", "lalo@example.com");
+            return contact.ByeContact();
+        }
+
+        public string HelloContact()
+        {
+            Contacts.Contact contact = new Contacts.Contact("Eduardo", "Rosas", "lalo@example.com");
+            return contact.GreetContact();
+        }
+    }
+}
