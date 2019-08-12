436e57d38d1e97faa17ede744f4ca616d37c3313
Noura Elarabi <NElarabi@integrant.com>
2019-08-12 19:23:23 +0300

    Section 12: Advanced Xamarin iOS

---
+++ ContactsApp/ContactsApp/ContactsApp.iOS/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ContactsApp.iOS;
+using ContactsLibraryiOS;
+using Foundation;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Contact))]
+namespace ContactsApp.iOS
+{
+    public class Contact : IContact
+    {
+        public Contact()
+        {
+        }
+
+        public string ByeContact()
+        {
+            ContactsLibraryiOS.ContactsApp contact = new ContactsLibraryiOS.ContactsApp((Foundation.NSString)"Eduardo", (Foundation.NSString)"Rosas", (Foundation.NSString)"lalo@example.com");
+            return contact.ByeContact();
+        }
+
+        public string HelloContact()
+        {
+            ContactsLibraryiOS.ContactsApp contact = new ContactsLibraryiOS.ContactsApp((Foundation.NSString)"Eduardo", (Foundation.NSString)"Rosas", (Foundation.NSString)"lalo@example.com");
+            return contact.GreetContact();
+        }
+    }
+}
